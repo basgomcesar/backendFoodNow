55ec0150c4f837942dba9e9340613e199c9d8c87
danimsix <dani.morales.sixto@gmail.com>
2024-11-24 18:05:59 -0600

    functionality change availability

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const multer = require('multer');
-const { save_usuario, get_usuario_by_id_params, update_usuario, delete_usuario, change_disponibility } = require('../controllers/usuarios');
+const { save_usuario, get_usuario_by_id_params, update_usuario, delete_usuario, update_availability } = require('../controllers/usuarios');
 const { validarJWT } = require('../helpers/validar-jwt');
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -12,6 +12,8 @@ router.get('/:idUsuario', validarJWT, get_usuario_by_id_params);
 router.post('/', upload.single('foto'), save_usuario);  // Aquí aplicamos el middleware de multer
 router.put('/:idUsuario', upload.single('foto'), validarJWT, update_usuario);
 router.delete('/:idUsuario', validarJWT, delete_usuario);
-router.patch('/:idUsuario', validarJWT, change_disponibility);
+router.put('/availability/:idUsuario', validarJWT, update_availability);
+
+
 
 module.exports = router;
