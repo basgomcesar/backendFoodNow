ac5040eb5aa06d236c706b2fd79d17e8dbba8d49
Caixba <zs21013850@estudiantes.uv.mx>
2024-12-03 18:15:57 -0600

    cambios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const multer = require('multer');
-const { save_usuario, get_usuario_by_id_params, update_usuario, delete_usuario, update_availability } = require('../controllers/usuarios');
+const { save_usuario, get_usuario_by_id_params, update_usuario, delete_usuario, update_availability, change_disponibility } = require('../controllers/usuarios');
 const { validarJWT } = require('../helpers/validar-jwt');
+
 
 const upload = multer({ storage: multer.memoryStorage() });
 
